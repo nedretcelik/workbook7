37cf7827b30d9715c2eed1e6edee020cbd90f84f
nedretcelik <nedretcelik97@gmail.com>
2024-06-04 10:12:55 -0400

    users table

---
+++ exercises/users-table/users.js
@@ -0,0 +1,28 @@
+"use strict";
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    console.log(users);
+    const usersTableBody = document.querySelector("#usersTableBody");
+    for (const user of users) {
+      let row = usersTableBody.insertRow();
+
+      let cell1 = row.insertCell();
+      cell1.innerText = user.name;
+
+      let cell2 = row.insertCell();
+      cell2.innerText = user.username;
+
+      let cell3 = row.insertCell();
+      cell3.innerText = user.email;
+
+      let cell4 = row.insertCell();
+      cell4.innerText = user.phone;
+
+      let cell5 = row.insertCell();
+      cell5.innerText = user.website;
+      let cell6 = row.insertCell();
+      cell6.innerText = user.company.name;
+    }
+  });
